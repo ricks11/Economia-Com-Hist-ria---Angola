7877dc596665dbf02510c61dfdc87debc28a89f1
Roger Jr <rogjunior2005@gmail.com>
2026-07-06 15:04:09 +0100

    ~~

--- ECHA.Web/Controllers/AuthController.fs
+++ ECHA.Web/Controllers/AuthController.fs
@@ -1,35 +1,32 @@
-    namespace ECHA.Web.Controllers
+namespace ECHA.Web.Controllers
 
 open Microsoft.AspNetCore.Mvc
 open System.Threading.Tasks
 open EconomiaComHistoria.Core.DTOs
-
 open Microsoft.AspNetCore.Authentication
 open Microsoft.AspNetCore.Authentication.Cookies
 open System.Security.Claims
 
-type AuthController (apiClient: ECHA.Web.Services.ApiClient) =
+type AuthController(apiClient: ECHA.Web.Services.ApiClient) =
     inherit Controller()
 
     [<HttpGet>]
-    member this.Login () =
+    member this.Login() =
         this.View()
 
     [<HttpPost>]
-    member this.Login (request: LoginRequestDto) =
+    member this.Login(request: LoginRequestDto) =
         task {
             let! authResponse = apiClient.LoginAsync(request)
             match authResponse with
             | Some response ->
-                let claims = [| 
-                    Claim(ClaimTypes.Name, response.Email)
-                    Claim(ClaimTypes.Role, response.Tipo)
-                    Claim("AccessToken", response.AccessToken)
-                |]
-                let claimsIdentity = ClaimsIdentity(claims, "CookieAuthentication")
+                let claims =
+                    [| Claim(ClaimTypes.Name, response.Email)
+                       Claim(ClaimTypes.Role, response.Tipo)
+                       Claim("AccessToken", response.AccessToken) |]
+                let claimsIdentity = ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme)
                 let claimsPrincipal = ClaimsPrincipal(claimsIdentity)
-                
-                do! this.HttpContext.SignInAsync("CookieAuthentication", claimsPrincipal)
+                do! this.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal)
                 return this.RedirectToAction("Index", "Home") :> IActionResult
             | None ->
                 this.ModelState.AddModelError("", "Login failed")
